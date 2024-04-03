6b0930b132eb18b4473c2d87eba2f87e705732a8
KLSchaper <klschaper@gmail.com>
2024-04-03 10:15:41 +0200

    added DAO call and fixed typing/formatting

--- src/view/elements/RecipePageElement.php
+++ src/view/elements/RecipePageElement.php
@@ -14,17 +14,19 @@ class RecipePageElement extends BaseElement
         // page title
         // page number
 
-    private BaseDAO $recipe_dao;
+    private \vrklk\model\recipe\RecipeDAO $recipe_dao;
     private array $recipe_details;
 
-    public function __construct(array $recipe_id_array, int $page_number, string $page_title)
-    {
-        //initialize DAOs required:
-        // -- RecipeDAO
-
+    public function __construct(
+        array $recipe_id_array,
+        int $page_number,
+        string $page_title
+    ) {
+        $this->recipe_dao = \ManKind\ModelManager::getRecipeDAO();
         $this->recipe_details = [];
         foreach ($recipe_id_array as $recipe_id) {
-            $this->recipe_details[] = $this->recipe_dao->getRecipeDetails($recipe_id);
+            $this->recipe_details[$recipe_id] = 
+                $this->recipe_dao->getRecipeDetails($recipe_id);
         }
     }
 
@@ -32,7 +34,7 @@ class RecipePageElement extends BaseElement
     {
         // h: page title
 
-        foreach ($this->recipe_details as $recipe_data) {
+        foreach ($this->recipe_details as $recipe_id => $recipe_data) {
             // img: recipe image (recipe_data['image'])
             // div:
                 // h: recipe name (recipe_data['title'])
