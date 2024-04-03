d83f6adaf82154dd2182443e6777687312a33a9d
KLSchaper <klschaper@gmail.com>
2024-04-03 16:34:37 +0200

    add css and script info to head

--- src/view/VPage.php
+++ src/view/VPage.php
@@ -15,6 +15,20 @@ class VPage extends \vrklk\base\view\HtmlDoc
     //=========================================================================
     // PROTECTED
     //=========================================================================
+    protected function showHeadContent(): void
+    {
+        echo <<<EOD
+        <title>$this->title</title>
+        <meta name="author" content="$this->author">
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="./assets/css/custom.css">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+        EOD;
+    }
+    
     protected function showBodyContent(): void
     {
         echo '<h1>Class ' . $this->title . '</h1>' . PHP_EOL;
