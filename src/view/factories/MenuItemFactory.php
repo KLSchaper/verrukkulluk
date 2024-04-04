8c9d39b1f2f29c7f84efdce832e8919b2b07d9db
KLSchaper <klschaper@gmail.com>
2024-04-04 14:38:39 +0200

    implement factory pattern for menu items

--- src/view/factories/MenuItemFactory.php
+++ src/view/factories/MenuItemFactory.php
@@ -4,8 +4,23 @@ namespace vrklk\view\factories;
 
 class MenuItemFactory extends \vrklk\base\view\BaseFactory
 {
-    public function create(): \vrklk\view\elements\MenuItemElement
-    {
-        return new \vrklk\view\elements\MenuItemElement;
+    public function create(
+        string $name,
+        array $properties
+    ): \vrklk\view\elements\MenuItemElement {
+        switch ($properties['type']) {
+            case 'link':
+                return new \vrklk\view\elements\MenuItemElement(
+                    $name,
+                    $properties['title'],
+                    $properties['display_order']
+                );
+            default:
+                return new \vrklk\view\elements\MenuItemElement(
+                    $name,
+                    $properties['title'],
+                    $properties['display_order']
+                );
+        }
     }
 }
