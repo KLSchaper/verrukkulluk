84968842fdee641f8170a3f00a03a09564f4e748
KLSchaper <klschaper@gmail.com>
2024-04-04 14:37:49 +0200

    add two input argument requirements to create()

--- src/base/view/BaseFactory.php
+++ src/base/view/BaseFactory.php
@@ -4,5 +4,8 @@ namespace vrklk\base\view;
 
 abstract class BaseFactory
 {
-    abstract public function create(): \vrklk\base\view\BaseElement;
+    abstract public function create(
+        string $name,
+        array $properties
+    ): \vrklk\base\view\BaseElement;
 }
