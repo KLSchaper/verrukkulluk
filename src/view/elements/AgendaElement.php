94fac21b5a5b2aac8e63c2b59570e859a27b8562
KLSchaper <klschaper@gmail.com>
2024-04-05 11:32:44 +0200

    add agenda element implementation to test page

--- src/view/elements/AgendaElement.php
+++ src/view/elements/AgendaElement.php
@@ -14,18 +14,35 @@ class AgendaElement extends \vrklk\base\view\BaseElement
 
     public function __construct()
     {
-        // get the agenda details from the agendaDAO for the next number of entries, and store it in the $agenda_data
+        $this->agenda_info = 
+            \ManKind\ModelManager::getAgendaDAO()->getUpcomingEvents(5);
     }
 
     public function show()
     {
-        // div:
-            //h: "Agenda"
+        echo <<<EOD
+        <div class="d-flex flex-column agenda" id="agenda-block">
+            <div class="text-center" style="color:var(--white)">
+                <h1 class="lily">Agenda</h1>
+            </div>
+        EOD . PHP_EOL;
         foreach($this->agenda_info as $event) {
-            //div:
-                //h: entry name
-                //p: entry date
-                //p: entry description
+            $date = date_create($event['date']);
+            echo <<<EOD
+                    <div class="container row">
+                        <div class="col-sm-3" style="color:var(--white)">
+                            <h3>{$date->format('d/m')}</h3>
+                            <p>{$date->format('H:i')}</p>
+                        </div>
+                        <div class="col-sm-9">
+                            <h3 class="lily">{$event['name']}</h3>
+                            <p>{$event['blurb']}</p>
+                        </div>
+                    </div>
+            EOD . PHP_EOL;
         }
+        echo <<<EOD
+        </div>
+        EOD . PHP_EOL;
     }
 }
