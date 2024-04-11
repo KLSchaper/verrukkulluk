f50f3b60a1f09fee69256f525ef368697e1d2918
KLSchaper <klschaper@gmail.com>
2024-04-11 17:12:36 +0200

    make handleGetRequest actually handle the GET request and make showResponse work with repurposed VPage

--- src/controller/VController.php
+++ src/controller/VController.php
@@ -19,12 +19,18 @@ class VController extends \vrklk\base\controller\Controller
             case 'detailtabs_test':
             case 'product_test':
             case 'recipe_test':
-                $this->response['page'] = 'test';
-                $this->response['title'] = 
-                    \vrklk\model\site\TestDAO::getTestTitle($this->request['page']);
-                $this->response['function_calls'] = 
-                    \vrklk\model\site\TestDAO::getTestFunctions($this->request['page']);
+                $this->response['page'] = 'dao_test';
+                $this->response['title'] = \vrklk\model\site\TestDAO::getTestTitle($this->request['page']);
+                $this->response['function_calls'] = \vrklk\model\site\TestDAO::getTestFunctions($this->request['page']);
                 break;
+            case 'form_test':
+                $this->response['title'] = 'TestForm';
+                break;
+            case 'home':
+                $this->response['title'] = 'Home';
+                break;
+            default:
+                $this->response['title'] = '404';
         }
     }
 
@@ -38,24 +44,37 @@ class VController extends \vrklk\base\controller\Controller
     protected function showResponse(): void
     {
         switch ($this->response['page']) {
-            case 'test':
-                $title = $this->response['title'];
+            case 'dao_test':
                 $main_element = new \vrklk\view\elements\DataElement(
                     $this->response['title'],
                     $this->response['function_calls'],
                 );
                 break;
             case 'form_test':
-                $page = new \vrklk\view\VPage('TestForm');
+                $main_element = new \vrklk\view\elements\FormElement(
+                    1,
+                    [
+                        'form_values' => [], 
+                        'form_errors' => []
+                    ]
+                );
                 break;
             
-            case 'page_test':
-                $page = new \vrklk\view\VPage('Page');
+            case 'home':
+                $page_number = 1; // TODO read from url request
+                $recipe_id_array = \ManKind\ModelManager::getRecipeDAO()->getHomeRecipes(4, $page_number);
+                $total_pages = ceil(\ManKind\ModelManager::getRecipeDAO()->getTotalHomeRecipes() / 4);
+                $main_element = new \vrklk\view\elements\RecipePageElement(
+                    recipe_id_array: $recipe_id_array,
+                    page_number: $page_number,
+                    total_pages: $total_pages,
+                );
                 break;
             default:
                 $page = new \vrklk\view\VPage('404');
         }
-        $page = new \vrklk\view\VPage($title, $main_element);
+        $user_id = 1; // TODO read from session
+        $page = new \vrklk\view\VPage($this->response['title'], $main_element, $user_id);
         $page->show();
     }
 
