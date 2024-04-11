f7264a2c015db5be4ae704126d18a25df2efadab
KLSchaper <klschaper@gmail.com>
2024-04-11 11:56:50 +0200

    replace font awesome stylesheet link that only worked for menu button with more generic font awesome javascript that adds more icons

--- src/view/VPage.php
+++ src/view/VPage.php
@@ -23,9 +23,9 @@ class VPage extends \vrklk\base\view\HtmlDoc
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <link rel="stylesheet" href="./assets/css/custom.css">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+        <script src="https://kit.fontawesome.com/83ed522242.js" crossorigin="anonymous"></script>
         EOD;
     }
 
