86194185d630facae7fcc90527996b90ba7038e9
KLSchaper <klschaper@gmail.com>
2024-04-15 16:41:46 +0200

    first working version of recipe details page, including all tabs

--- src/view/elements/RecipeDetailsElement.php
+++ src/view/elements/RecipeDetailsElement.php
@@ -6,11 +6,27 @@ class RecipeDetailsElement extends \vrklk\base\view\BaseElement
 {
     private array $recipe_details;
     private bool $is_favorite;
+    private string $ingredients_tab_title;
+    private string $prep_steps_tab_title;
+    private string $comments_tab_title;
+    private array $ingredients_tab_content;
+    private array $prep_steps_tab_content;
+    private array $comments_tab_content;
 
+    //=========================================================================
+    // PUBLIC
+    //=========================================================================
     public function __construct(int $recipe_id, int $user_id)
     {
         $this->recipe_details = \ManKind\ModelManager::getRecipeDAO()->getRecipeDetails($recipe_id);
         $this->is_favorite = \ManKind\ModelManager::getFavoritesDAO()->checkFavorite($recipe_id, $user_id);
+
+        $this->ingredients_tab_title = \ManKind\ModelManager::getIngredientsTabDAO()->getTabName();
+        $this->prep_steps_tab_title = \ManKind\ModelManager::getPrepStepsTabDAO()->getTabName();
+        $this->comments_tab_title = \ManKind\ModelManager::getCommentsTabDAO()->getTabName();
+        $this->ingredients_tab_content = \ManKind\ModelManager::getIngredientsTabDAO()->getTabContent($recipe_id);
+        $this->prep_steps_tab_content = \ManKind\ModelManager::getPrepStepsTabDAO()->getTabContent($recipe_id);
+        $this->comments_tab_content = \ManKind\ModelManager::getCommentsTabDAO()->getTabContent($recipe_id);
     }
 
     public function show()
@@ -37,9 +53,21 @@ class RecipeDetailsElement extends \vrklk\base\view\BaseElement
                 // p: recipe description (from recipe data)
             //div:
                 // button: "op lijst" (put ingredients in shopping list)
+        if ($this->is_favorite) {
+            $heart = 'fa-solid';
+        } else {
+            $heart = 'fa-regular';
+        }
+
+        $ingredients_tab = $this->makeIngredientsTabContent($this->ingredients_tab_content);
+        $prep_steps_tab = $this->makePrepStepsTabContent($this->prep_steps_tab_content);
+        $comments_tab = $this->makeCommentsTabContent($this->comments_tab_content);
+
         echo <<<EOD
         <div id="detail-page">
-            <div class="detail-main row m-3">
+
+            <!-- Detail block main -->
+            <div class="detail-main row my-3 shadow">
                 <div class="col-sm-5">
                     <img class="img-fluid" src="./assets/img/recipes/{$this->recipe_details['img']}" alt="{$this->recipe_details['title']}">
                 </div>
@@ -58,23 +86,127 @@ class RecipeDetailsElement extends \vrklk\base\view\BaseElement
                     <div class="row">
                         <div class="col-sm-8">
                             <h1 class="green-lily text-truncate">{$this->recipe_details['title']}</h1>
+                            <div class="d-flex align-items-center">
+                                <div><h4 class="green-lily my-auto">Keuken: </h4></div>
+                                <div><p class="my-auto ps-1">{$this->recipe_details['cuisine']}</p></div>
+                            </div>
                         </div>
                         <div class="col-sm-4">
                             Rating: {$this->recipe_details['rating']}/10
+                            <div class="d-flex align-items-center mt-auto">
+                                <div><h4 class="green-lily my-auto">Type: </h4></div>
+                                <div><p class="my-auto ps-1">{$this->recipe_details['display']}</p></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="py-2">
+                        <p>{$this->recipe_details['descr']}</p>
+                    </div>
+                    <div class="d-flex align-items-center">
+                        <button type="button" class="btn btn-card p-0">
+                            <h1 class="m-0"><span class="badge rounded-pill red-white-lily">Op Lijst</span></h1>
+                        </button>
+                        <div class="ms-auto">
+                            <h2><i class="{$heart} fa-heart red me-4" id="favorite-heart"></i></h2>
                         </div>
                     </div>
                 </div>
-        EOD . PHP_EOL;
-        if ($this->is_favorite) {
-                // button: filled-in heart symbol (removes favorite)
-        } else {
-                // button: empty heart symbol (makes favorite)
-        }
-        echo <<<EOD
             </div>
-            <div class="detail-tab">
+
+            <!-- Detail tabs -->
+            <div class="detail-tab my-3 shadow">
+                <ul class="nav nav-tabs">
+                    <li class="nav-item">
+                        <a class="nav-link active" data-bs-toggle="tab" href="#ingredients">
+                            <h3><span class="green-lily">{$this->ingredients_tab_title}</span></h3>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" data-bs-toggle="tab" href="#prep_steps">
+                            <h3><span class="green-lily">{$this->prep_steps_tab_title}</span></h3>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" data-bs-toggle="tab" href="#comments">
+                            <h3><span class="green-lily">{$this->comments_tab_title}</span></h3>
+                        </a>
+                    </li>
+                </ul>
+
+                <!-- Tab panes -->
+                <div class="tab-content overflow-auto" style="height: 400px">
+                    <div class="tab-pane container active" id="ingredients">
+                        <div class="row">{$ingredients_tab}</div>
+                    </div>
+                    <div class="tab-pane container fade" id="prep_steps">
+                        <div class="row">{$prep_steps_tab}</div>
+                    </div>
+                    <div class="tab-pane container fade" id="comments">
+                        <div class="row">{$comments_tab}</div>
+                    </div>
+                </div>
             </div>
         </div>
         EOD . PHP_EOL;
+    }
+
+    //=========================================================================
+    // PRIVATE
+    //=========================================================================
+    private function makeIngredientsTabContent($ingredients): string {
+        $tab_content = '';
+        foreach($ingredients as $ingredient) {
+            $name = ucfirst($ingredient['ingredient']);
+            $tab_content .=
+            <<<EOD
+            <div class="col-sm-3">
+                {$ingredient['img']}
+            </div>
+            <div class="col-sm-9">
+                <h3 class="green-lily">{$name}</h3>
+                <p>{$ingredient['blurb']}</p>
+                <div class="d-flex align-items-center">
+                    <div><h4 class="green-lily my-auto">Hoeveelheid: </h4></div>
+                    <div><p class="my-auto ps-1">{$ingredient['quantity']} {$ingredient['measure']}</p></div>
+                </div>
+            </div>
+            EOD . PHP_EOL;
+        }
+        return $tab_content;
+    }
+
+    private function makePrepStepsTabContent($prep_steps): string {
+        $tab_content = '';
+        foreach($prep_steps as $prep_step) {
+            $tab_content .= 
+            <<<EOD
+            <div class="col-sm-2">
+                <div class="m-auto ps-number">
+                    <h1>{$prep_step['number']}</h1>
+                </div>
+            </div>
+            <div class="col-sm-10">
+                <p>{$prep_step['descr']}</p>
+            </div>
+            EOD . PHP_EOL;
+        }
+        return $tab_content;
+    }
+
+    private function makeCommentsTabContent($comments): string {
+        $tab_content = '';
+        foreach($comments as $comment) {
+            $tab_content .= 
+            <<<EOD
+            <div class="col-sm-3">
+                {$comment['img']}
+            </div>
+            <div class="col-sm-8">
+                <h3 class="green-lily">{$comment['name']}</h3>
+                <p>{$comment['text']}</p>
+            </div>
+            EOD . PHP_EOL;
+        }
+        return $tab_content;
     }
 }
