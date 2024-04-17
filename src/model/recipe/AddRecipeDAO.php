59f25c3b0140702ce4e9ad5a1eff831d095244c6
KLSchaper <klschaper@gmail.com>
2024-04-17 14:58:06 +0200

    add storeRecipe transaction logic and implement addRecipe

--- src/model/recipe/AddRecipeDAO.php
+++ src/model/recipe/AddRecipeDAO.php
@@ -2,29 +2,38 @@
 
 namespace vrklk\model\user;
 
+use PDOException;
+
 class AddRecipeDAO extends \vrklk\base\model\BaseDAO implements
     \vrklk\model\interfaces\iAddMeasure,
     \vrklk\model\interfaces\iAddRecipe
 {
+    //=========================================================================
+    // PUBLIC
+    //=========================================================================
     /*  EXPECTED VALUES:
         
         $recipe_values = [
             'name' => 'stamppot',
             'img' => 'stamppot.webp',
             'blurb' => 'lekker',
-            ...etc
+            'people' => 4,
+            'cuisine_id' => 6,
+            'type' => 'meat',
+            'descr' => 'een winterse klassieker',
+            'user_id' => 1
         ]
 
         $ingredient_values = [
             0 => [
-                'ingredient_id' => 1,
+                'ingredient_id' => 9,
                 'quantity' => 3.14,
-                'measure_id' => 1
+                'measure_id' => 2
             ],
             1 => [
-                'ingredient_id' => 1,
-                'quantity' => 3.14,
-                'measure_id' => 1
+                'ingredient_id' => 5,
+                'quantity' => 2.71,
+                'measure_id' => 7
             ],
             ...etc
         ]
@@ -41,7 +50,22 @@ class AddRecipeDAO extends \vrklk\base\model\BaseDAO implements
         array $ingredient_values,
         array $prep_step_values
     ): int|false {
-        return 0;
+        try {
+            $this->crud->beginTransaction();
+            $recipe_id = $this->addRecipe(...$recipe_values);
+            foreach ($ingredient_values as $ingredient) {
+                $this->addRecipeIngredient($recipe_id, ...$ingredient);
+            }
+            foreach ($prep_step_values as $step_number => $step_descr) {
+                $this->addRecipeStep($recipe_id, $step_number, $step_descr);
+            }
+            $this->crud->commit();
+            return $recipe_id;
+        } catch (PDOException $e) {
+            $this->crud->rollBack();
+            \ManKind\tools\dev\Logger::_error($e);
+            return false;
+        }
     }
 
     public function addMeasure(
@@ -63,12 +87,26 @@ class AddRecipeDAO extends \vrklk\base\model\BaseDAO implements
         int $people,
         int $cuisine_id,
         string $type,
-        string $descr
+        string $descr,
+        int $user_id,
     ): int|false {
-        return 0; // TODO implement function
+        return $this->crud->doInsert(
+            "INSERT INTO recipes (name, img, blurb, people, cuisine_id, type, descr, user_id)"
+                . " VALUES (:name, :img, :blurb, :people, :cuisine_id, :type, :descr, :user_id)",
+            [
+                'name' => [$name, false],
+                'img' => [$img, false],
+                'blurb' => [$blurb, false],
+                'people' => [$people, true],
+                'cuisine_id' => [$cuisine_id, true],
+                'type' => [$type, false],
+                'descr' => [$descr, false],
+                'user_id' => [$user_id, true],
+            ]
+        );
     }
 
-    private function storeRecipeIngredient(
+    private function addRecipeIngredient(
         int $recipe_id,
         int $ingredient_id,
         float $quantity,
@@ -77,7 +115,7 @@ class AddRecipeDAO extends \vrklk\base\model\BaseDAO implements
         return false; // TODO implement function
     }
 
-    private function storeRecipeStep(
+    private function addRecipeStep(
         int $recipe_id,
         int $number,
         string $descr
