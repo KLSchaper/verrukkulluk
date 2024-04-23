11e644f16915c060a46fba9a1fe2c7c334135bc0
KLSchaper <klschaper@gmail.com>
2024-04-23 13:55:26 +0200

    implement base page handling fucntionality

--- src/base/controller/BasePageHandler.php
+++ src/base/controller/BasePageHandler.php
@@ -5,5 +5,24 @@ namespace vrklk\base\controller;
 abstract class BasePageHandler extends \vrklk\base\controller\RESTfulHandler
 
 {
-    // TODO
+    protected string $requested_page;
+
+    //=========================================================================
+    // PROTECTED
+    //=========================================================================
+    protected function _generateResponse(): bool
+    {
+        $this->requested_page = \vrklk\base\controller\Request::getRequestVar(
+            key: 'page',
+            default: 'home',
+        );
+        return $this->_createPage();
+    }
+
+    protected function _reportError(\Throwable $e): void
+    {
+        \ManKind\tools\dev\Logger::_error($e);
+    }
+
+    abstract protected function _createPage(): bool;
 }
