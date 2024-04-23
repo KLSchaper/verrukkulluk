884b1eeb87975868bfbb0d2b6157a9ab0de862f4
KLSchaper <klschaper@gmail.com>
2024-04-23 10:58:12 +0200

    add handler factory

---
+++ src/base/controller/HandlerFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace vrklk\base\controller;
+
+class HandlerFactory
+{
+    public function createHandler(): \vrklk\interfaces\iRequestHandler
+    {
+        $requested_handler = 'page'; // TODO replace with getRequestVar function somehow
+        \ManKind\tools\dev\Logger::_echo($requested_handler . '-call made');
+        switch ($requested_handler) {
+            case 'page':
+                return new \vrklk\controller\PageHandler();
+            case 'ajax':
+                return new \vrklk\controller\AjaxHandler();
+            default:
+                throw new \Exception('Unknown handler requested: '
+                    . $requested_handler);
+        }
+    }
+}
