6ca62e8d104fa7b33ae1390e838fdc8211280a92
KLSchaper <klschaper@gmail.com>
2024-04-23 12:19:42 +0200

    implement handleRequest structure

--- src/base/controller/RESTfulHandler.php
+++ src/base/controller/RESTfulHandler.php
@@ -5,8 +5,28 @@ namespace vrklk\base\controller;
 abstract class RESTfulHandler implements \vrklk\interfaces\iRequestHandler
 
 {
+    //=========================================================================
+    // PUBLIC
+    //=========================================================================
     final public function handleRequest(): bool
     {
-        return false;
+        $result = false;
+        try {
+            ob_start();
+            if ($this->_generateResponse()) {
+                echo ob_get_clean();
+                $result = true;
+            }
+        } catch (\Throwable $ex) {
+            ob_end_clean();
+            $this->_reportError($ex);
+        }
+        return $result;
     }
+
+    //=========================================================================
+    // PROTECTED
+    //=========================================================================
+    abstract protected function _generateResponse(): bool;
+    abstract protected function _reportError(\Throwable $ex): void;
 }
