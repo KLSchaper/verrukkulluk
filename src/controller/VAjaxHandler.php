0a99dfa4b462ae090e6c0f81ac541d05a735d544
KLSchaper <klschaper@gmail.com>
2024-04-23 11:24:26 +0200

    add page and ajax handler classes

---
+++ src/controller/VAjaxHandler.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace vrklk\controller;
+
+abstract class VAjaxHandler extends \vrklk\base\controller\BaseAjaxHandler
+
+{
+    // TODO
+}
---
+++ src/controller/VPageHandler.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace vrklk\controller;
+
+abstract class VPageHandler extends \vrklk\base\controller\BasePageHandler
+
+{
+    // TODO
+}
