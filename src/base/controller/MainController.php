ea11f4157e9cf080b71e1c46b31df2a927b3c830
KLSchaper <klschaper@gmail.com>
2024-04-23 10:48:26 +0200

    add main controller class

---
+++ src/base/controller/MainController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace vrklk\base\controller;
+
+class MainController
+{
+    protected $handler_factory;
+
+    public function __construct(\vrklk\base\controller\HandlerFactory $handler_factory)
+    {
+        $this->handler_factory = $handler_factory;
+    }
+
+    public function handleRequest(): bool
+    {
+        $dispatch_to = $this->handler_factory->createHandler();
+        if ($dispatch_to)
+        {
+            return $dispatch_to->handleRequest();
+        }
+        return false;
+    }
+}
