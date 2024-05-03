2935d7f457747d1db500d9fdb354a3a345fe2b7e
KLSchaper <klschaper@gmail.com>
2024-05-03 14:17:00 +0200

    add functions to add and remove favorites

--- src/model/interfaces/iFavoritesDAO.php
+++ src/model/interfaces/iFavoritesDAO.php
@@ -5,4 +5,6 @@ namespace vrklk\model\interfaces;
 interface iFavoritesDAO
 {
     public function checkFavorite(int $recipe_id, int $user_id): bool;
+    public function addFavorite(int $recipe_id, int $user_id): int|false;
+    public function removeFavorite(int $recipe_id, int $user_id): int|false;
 }
--- src/model/user/FavoritesDAO.php
+++ src/model/user/FavoritesDAO.php
@@ -21,4 +21,29 @@ class FavoritesDAO extends \vrklk\base\model\BaseDAO implements
         );
         return boolval($favorite);
     }
+
+    public function addFavorite(int $recipe_id, int $user_id): int|false
+    {
+        return $this->crud->doInsert(
+            "INSERT INTO favorites (recipe_id, user_id)"
+                . " VALUES (:recipe_id, :user_id)",
+            [
+                'recipe_id' => [$recipe_id, true],
+                'user_id' => [$user_id, true],
+            ]
+        );
+    }
+
+    public function removeFavorite(int $recipe_id, int $user_id): int|false
+    {
+        return $this->crud->doDelete(
+            "DELETE FROM favorites"
+                . " WHERE recipe_id = :recipe_id"
+                . " AND user_id = :user_id",
+            [
+                'recipe_id' => [$recipe_id, true],
+                'user_id' => [$user_id, true],
+            ]
+        );
+    }
 }
