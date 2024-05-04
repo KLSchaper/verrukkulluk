f6c7c55bc19ac65f9b5fcb2367b1333256c3e563
KLSchaper <klschaper@gmail.com>
2024-05-04 20:29:49 +0200

    redo changeFavoriteStatus to work with response codes, implement addToShoppingList following the same setup

--- assets/js/ajax_functions.js
+++ assets/js/ajax_functions.js
@@ -1,5 +1,5 @@
 /* FAVORITE */
-function changeFavoriteStatus() {
+const changeFavoriteStatus = () => {
     const heart = document.getElementById('favorite-heart');
     const url = heart.getAttribute('data-vrklk-fav-url');
 
@@ -11,15 +11,47 @@ function changeFavoriteStatus() {
 
     let xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            if (xmlhttp.responseText === '') {
-                gwprompt.show(not_logged_favorite_options());
-            } else {
-                gwprompt.show(toggle_favorite_options(xmlhttp.responseText));
-                heart.classList.toggle('fa-regular');
-                heart.classList.toggle('fa-solid');
+        if (xmlhttp.readyState == 4) {
+            switch (xmlhttp.status) {
+                case 200:
+                    gwprompt.show(toggle_favorite_options(xmlhttp.responseText));
+                    heart.classList.toggle('fa-regular');
+                    heart.classList.toggle('fa-solid');
+                    break;
+                case 401:
+                    gwprompt.show(not_logged_options(xmlhttp.responseText));
+                    break;
+                case 500:
+                    gwprompt.show(action_failed_options(xmlhttp.responseText));
+                    break;
+                default:
+                    gwprompt.show(action_failed_options(xmlhttp.status));
             }
-        }
+        } 
+    };
+    xmlhttp.open("GET", url);
+    xmlhttp.send();
+}
+
+/* ADD TO LIST */
+const addToShoppingList = () => {
+    const add_button = document.getElementById('add-to-list');
+    const url = add_button.getAttribute('data-vrklk-add-list-url');
+    
+    let xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+            switch (xmlhttp.status) {
+                case 200:
+                    gwprompt.show(add_to_list_options(xmlhttp.responseText));
+                    break;
+                case 500:
+                    gwprompt.show(action_failed_options(xmlhttp.responseText));
+                    break;
+                default:
+                    gwprompt.show(action_failed_options(xmlhttp.statusText));
+            }
+        } 
     };
     xmlhttp.open("GET", url);
     xmlhttp.send();
@@ -29,6 +61,7 @@ function changeFavoriteStatus() {
 const initAJAXPage = () => {
     console.log('initAJAXPage');
     document.getElementById('favorite-heart').addEventListener('click', changeFavoriteStatus);
+    document.getElementById('add-to-list').addEventListener('click', addToShoppingList);
 }
 
 (function (fn) {
